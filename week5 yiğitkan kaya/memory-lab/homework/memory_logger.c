f4dcdf5a889e62e7e0528ece8f31a0cab5f3b95b
Yiğitkan kaya <265235339+YIGITKANKAYA@users.noreply.github.com>
2026-05-01 14:57:02 +0200

    Add files via upload

---
+++ week5 yiğitkan kaya/memory-lab/homework/memory_logger.c
@@ -0,0 +1,83 @@
+// Homework – Memory Logger
+// Name: _______________
+// Date: _______________
+//
+// Read the README.md in this folder before starting!
+
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NUM_WORDS 5
+
+int main(void)
+{
+    // TODO: Declare an array of NUM_WORDS char* pointers
+    char *words[NUM_WORDS];
+
+    // -------------------------------------------------------
+    // STEP 1 & 2: Read words and store copies in memory
+    // -------------------------------------------------------
+    for (int i = 0; i < NUM_WORDS; i++)
+    {
+        char *input = get_string("Enter word %i: ", i + 1);
+
+        // TODO: Allocate memory for a copy of input (+1 for '\0')
+        wwords[i] = malloc(strlen(input) + 1);
+
+        // TODO: Check if malloc returned NULL
+       if (words[i] == NULL)
+        {
+            printf("Memory error!\n");
+            return 1;
+        }
+
+        // TODO: Copy input into words[i]
+        strcpy(words[i], input);
+
+        // TODO: Capitalize the first letter of words[i]
+        if (strlen(words[i]) > 0)
+        {
+            t[0] = toupper(words[i][0]);
+        }
+    }
+
+    // -------------------------------------------------------
+    // STEP 3: Save all words to log.txt
+    // -------------------------------------------------------
+
+    // TODO: Open "log.txt" in write mode "w"
+    FILE *file = fopen("log.txt", "w");
+
+    // TODO: Check if fopen returned NULL
+    if (file == NULL)
+    {
+        printf("Could not open file!\n");
+        return 1;
+    }
+
+    // TODO: Write each word to the file followed by "\n"
+    for (int i = 0; i < NUM_WORDS; i++)
+    {
+        fprintf(file, "%s\n", words[i]);
+    }
+
+    // TODO: Close the file
+   fclose(file);
+
+    printf("\nWords saved to log.txt!\n");
+
+    // -------------------------------------------------------
+    // STEP 4: Free all allocated memory
+    // -------------------------------------------------------
+
+    // TODO: Free each word
+    for (int i = 0; i < NUM_WORDS; i++)
+    {
+        free(words[i]);
+    }
+
+    return 0;
+}
